253be2fe2980c69b4d99032d13a1be58d671a0e3
Huxley_Millard <gerogiamillardccm@gmail.com>
2019-04-13 17:28:58 +0100

    REGISTERFORM: added basic form

--- unwrapped/src/components/register.js
+++ unwrapped/src/components/register.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import RegisterForm from './register_form';
+
 
 class Register extends Component{
+ 
     render(){
-        return (<h1> Register</h1>);
+        return (<div>
+              <h1> Register</h1>
+              <RegisterForm/>
+            </div>);
     }
 }
 
