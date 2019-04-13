52d93d8377cdf3312ed476215ba838ada919e5e9
Carol Gardiner <student@Makerss-MacBook-Pro.local>
2019-04-13 17:59:33 +0100

    attempt at fetch to google api

---
+++ unwrapped/src/components/Address/AddressSearch.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class AddressSearch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: []
+    }
+  }
+  componentDidMount(){
+    fetch("")
+  }
+  render() {
+   
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
