5767520c4f87eaac6d3e6428801dc91a2eb1681c
Huxley_Millard <gerogiamillardccm@gmail.com>
2019-04-14 10:40:00 +0100

    GEOLOCATION:front end set up

--- unwrapped/src/components/find.js
+++ unwrapped/src/components/find.js
@@ -3,6 +3,46 @@ import MapContainer from './map'
 import SearchBar from './searchbar'
 
 class Find extends Component{
+        constructor(props) {
+          super(props)
+          this.state = {
+            venues: []
+            }
+          }
+      
+        geolocateMe = () => {
+            if (navigator.geolocation) {
+              navigator.geolocation.getCurrentPosition(this.getLatandLong);
+            } else {
+              console.log("Geolocation is not supported by this browser.");
+            }
+          }
+      
+        getLatandLong = (position) => {
+            let location =  {location : {lat : position.coords.latitude.toFixed(2),  long : position.coords.longitude.toFixed(2)}};
+            const body  = JSON.stringify(location)
+            fetch('https://enigmatic-badlands-83570.herokuapp.com/api/v1/venues', {
+              method: 'POST',
+              headers:{
+                'Content-Type': 'application/json'
+              },
+              body: body
+            })
+            .then((response) => {
+              return response.json()
+            })
+            .then((response) => {
+              this.setState({
+                venues: response
+              })
+            })
+          }
+      
+         componentDidMount() {
+          this.geolocateMe();
+        }
+
+    
     render(){
         return (
         <div>
