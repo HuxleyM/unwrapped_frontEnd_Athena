2b3cf7497d74fc815cc7ac5cb70bdd912e5a2807
Huxley_Millard <gerogiamillardccm@gmail.com>
2019-04-14 12:32:51 +0100

    reverted back to an original version

--- unwrapped/src/components/register_form_dropdown.js
+++ unwrapped/src/components/register_form_dropdown.js
@@ -1,57 +1,52 @@
 import React, { Component } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown'
 
-
 class FormDropdown extends Component {
-    constructor() {
-      super();
-      this.state = {
-        selectedItems: [] 
-      };
-    }
-    
-    itemSelected(item){
-      if(this.state.selectedItems.includes(item)){
-        
-      }else{
-        var itemsList=this.state.selectedItems.push(item)
-        this.setState({
-          selected: itemsList
-        })
-      }
-    }
-    
+  constructor() {
+    super();
+    this.state = {
+      selectedItems: [] 
+    };
+  }
 
-    printPretty(){
-      return (<div>
-        { this.state.selectedItems.forEach((item)=>{
-          <button>item</button>})
-        }
-      </div>)
-    }
-   
-    render() {
-      return (
-          <div>
-            <Dropdown onSelect={(eventKey: any, event: Object)=>{ this.itemSelected(eventKey)}}>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-                Dropdown Button
-            </Dropdown.Toggle>
+  itemSelected(item){
+  // this.state.selectedItems.find(item) ? 'jk' : 'huj'
+  var itemsList=this.state.selectedItems.push(item)
+    this.setState({
+      selected: itemsList
+    })
+  }
 
-            <Dropdown.Menu>
-                <Dropdown.Item eventKey="fruit">Fruit</Dropdown.Item>
-				<Dropdown.Item eventKey="veg">Veg</Dropdown.Item>
-                <Dropdown.Item eventKey="shampoo">Shampoos</Dropdown.Item>
-                <Dropdown.Item eventKey="dry-goods">Dry Goods</Dropdown.Item>
-				<Dropdown.Item eventKey="wine">Wine</Dropdown.Item>
-            </Dropdown.Menu>
-        </Dropdown>
-		<hr></hr> 
-		<h3>Selected</h3>
-		<div>{this.printPretty()}</div>
-          </div>
-      )
-    }
+
+  printPretty(){
+    console.log('hi')
+      return this.state.selectedItems.join('\n');
+  }
+
+  // <button onClick={()=>{this.changeUserJourney('find')}}>find a store</button>
+// 
+  render() {
+    return (
+        <div>
+          <Dropdown onSelect={(eventKey: any, event: Object)=>{ this.itemSelected(eventKey)}}>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Dropdown Button
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+              <Dropdown.Item eventKey="fruit">Fruit</Dropdown.Item>
+      <Dropdown.Item eventKey="veg">Veg</Dropdown.Item>
+              <Dropdown.Item eventKey="shampoo">Shampoos</Dropdown.Item>
+              <Dropdown.Item eventKey="dry-goods">Dry Goods</Dropdown.Item>
+      <Dropdown.Item eventKey="wine">Wine</Dropdown.Item>
+          </Dropdown.Menu>
+      </Dropdown>
+  <hr></hr> 
+  <h3>Selected</h3>
+  <div>{this.printPretty()}</div>
+        </div>
+    )
+  }
 }
 
 export default FormDropdown
